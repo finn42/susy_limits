2c4676f5ed40a33055faeb1daf652c0c34cd7113
Finn Upham <finn.upham@gmail.com>
2024-01-26 13:13:25 +0100

    Add plots and minor edits

--- susy.py
+++ susy.py
@@ -32,13 +32,14 @@ def sig_seg(signal,seg_size): # in samples
     return signal_segments
 
 
-def plot_zxcorr_pairs(A,B,maxlag):
+def plot_zxcorr_pairs(A,B,maxlag,plottag=''):
     
     fig, axs = plt.subplots(2,2,figsize=(7,7))
     axs[0,0].plot(A)
     axs[1,0].plot(B)
-    axs[0,0].set_title('Signal A segment')
-    axs[1,0].set_title('Signal B segment')
+    axs[0,0].set_title('Signal segments')
+    axs[0,0].set_ylabel('Signal A segment')
+    axs[1,0].set_ylabel('Signal B segment')
     #CCC = axs[0,1].xcorr(A, B, usevlines=True, maxlags=maxlag, normed=True, lw=3)
     CCC = XCORR(A,B,maxlag)
     axs[0,1].fill_between(np.arange(-maxlag,maxlag+1),CCC,label = 'Fishers Z')
@@ -49,6 +50,17 @@ def plot_zxcorr_pairs(A,B,maxlag):
     axs[1,1].legend()
     axs[1,1].set_ylim([-3,3])
     axs[1,1].set_title('Fishers Z and Average')
+    axs[1,0].set_xlabel('Samples')
+    axs[1,1].set_xlabel('Lags') 
+    
+    if len(plottag)>0:
+        a = plottag.split('/')
+        if '/' in plottag:
+            a = plottag.split('/')
+            tag = a[-1]
+        else: tag = plottag
+        axs[0,0].set_title(tag)
+        plt.savefig(plottag + 'ZXCORR_Pair.png', bbox_inches = 'tight')
     plt.show()
     
     return np.arctanh(CCC).mean()
@@ -72,21 +84,6 @@ def XCORR(segA,segB,maxlag):
     for k in range(1,maxlag+1): 
         ccc[k] = sp.stats.pearsonr(segA[:-k], segB[k:]).statistic
     return ccc
-
-# def zxcorr_sims(sig_A_segments,sig_B_segments,maxlags):
-#     segment_sims = pd.DataFrame(columns = ['A_seg','B_seg','zxcorr','set'])
-#     k = 0
-#     for i in range(n):
-#         for j in range(n):
-#             segment_sims.loc[k,'A_seg'] = i
-#             segment_sims.loc[k,'B_seg'] = j
-#             segment_sims.loc[k,'zxcorr'] = ZXCORR(sig_A_segments[i],sig_B_segments[j],maxlags)
-#             if i==j:
-#                 segment_sims.loc[k,'set'] = 'Real'
-#             else:
-#                  segment_sims.loc[k,'set'] = 'Sup'
-#             k+=1
-#     return segment_sims 
 
 def zxcorr_sims(sig_A_segments,sig_B_segments,maxlags):
     segment_sims = {}
